d12895daf6c1c012976ce15715e6c463d4d703ba
Mammen Mathew <mammen999@gmail.com>
2025-04-25 04:00:29 +0530

    fix: update axios baseURL to use the production server

--- src/Services/Axios/axios.ts
+++ src/Services/Axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  baseURL: "https://travel-package-booking-task-server.onrender.com/api", 
   headers: {
     "Content-Type": "application/json",
   },
